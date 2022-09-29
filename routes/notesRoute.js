a2dd8d84334200692e70e5fe58c290279891e50d
Savannah McGinnis <sjmhorsegirl@verizon.net>
2022-09-28 19:15:46 -0500

    notes Route

--- routes/notesRoute.js
+++ routes/notesRoute.js
@@ -1,9 +1,38 @@
 const notesRoute = require('express').Router();
+const uuid = require('../Develop/db/uuid');
 
 // const { readFromFile, readAndAppend } = require('../Develop/db/db.json');
 
 notesRoute.get('/', (req, res) => {
-    console.info(`${req.method} request received for feedback`);
+    console.info(`${req.method} notes have been recieved`);
   
-    readFromFile('./db/feedback.json').then((data) => res.json(JSON.parse(data)));
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
   });
+
+  // POST Route for notes
+  notesRoute.post('/', (req,res) => {
+    console.info(`${req.method} request to submit notes`);
+
+    // destructuring
+    const { title, text } = req.body;
+    if (title && text) {
+        // note object to be saved
+        const newNote = {
+            title,
+            text,
+            id: uuid,
+        };
+        readAndAppend(newNote, '../db/db.json');
+
+        const response = {
+            status: 'IT WORKED',
+            body: newNote,
+        };
+
+        res.json(response);
+    }else{
+        res.json('ERROR IN POST');
+    }
+  });
+
+  module.exports = notesRoute;
